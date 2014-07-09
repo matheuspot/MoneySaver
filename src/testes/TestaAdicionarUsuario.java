7726801a8985519bf69b977aef1d6a2df675250c
Flábio Fagundes <flabio.fagundes@gmail.com>
2014-07-09 09:29:18 -0300

    Teste e fonte do Adicionar Usuario
    Criados os arquivos de teste e fonte do Adicionar Usuario.

---
+++ src/testes/TestaAdicionarUsuario.java
@@ -0,0 +1,14 @@
+package testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestaAdicionarUsuario {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
