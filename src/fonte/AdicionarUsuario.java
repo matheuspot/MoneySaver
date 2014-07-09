c75d3ea1bf75535a74aec681930e57d0e2e5feca
Flábio Fagundes <flabio.fagundes@gmail.com>
2014-07-09 11:10:37 -0300

    Implementando o AdicionarUsuario

--- src/fonte/AdicionarUsuario.java
+++ src/fonte/AdicionarUsuario.java
@@ -1,5 +1,14 @@
 package fonte;
 
 public class AdicionarUsuario {
+	private String nome, email, senha, dicaSenha;
+	
+	public AdicionarUsuario(String email, String nome, String senha, String dicaSenha){
+		this.email = email;
+		this.nome = nome;
+		this.senha = senha;
+		this.dicaSenha = dicaSenha;
+		
+	}
 
 }
