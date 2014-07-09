dcc19428e28b5bf958e5521fd605fc837469f9f4
Roberto Matheus <matheus.pot@gmail.com>
2014-07-09 20:56:49 -0300

    Mudanças em Usuario
    Alteração do método validaEmail para private, mudanças na lógica e
    mensagens de erro.

--- src/fonte/Usuario.java
+++ src/fonte/Usuario.java
@@ -5,32 +5,35 @@ import java.util.regex.*;
 public class Usuario {
 
 	private String nome, email, senha, dicaSenha;
-	
-	public Usuario(String nome, String email, String senha, String dicaSenha) throws Exception {
+
+	public Usuario(String nome, String email, String senha, String dicaSenha)
+			throws Exception {
 		if ((nome.equals("")) || (nome == null))
-			throw new Exception("Nome nao definido.");
-		
+			throw new Exception("Nome não definido.");
+
 		if ((senha.length() < 6) || (senha.length() > 8))
-			throw new Exception("Senha com tamanho invalido.");
-		
+			throw new Exception("Senha com tamanho inválido.");
+
 		if (!validaEmail(email))
-			throw new Exception("Email invalido.");
-		
+			throw new Exception("Email inválido.");
+
 		this.nome = nome;
 		this.email = email;
 		this.senha = senha;
 		this.dicaSenha = dicaSenha;
 	}
-	
-	public boolean validaEmail(String email) {
-		if (email != null && email.length() > 0) 
+
+	private boolean validaEmail(String email) {
+		if (email == null || email.length() < 0)
 			return false;
-		
-	    Pattern p = Pattern.compile("^[\\w-]+(\\.[\\w-]+)*@([\\w-]+\\.)+[a-zA-Z]{2,7}$"); 
-	    Matcher m = p.matcher(email); 
-	    if (m.find())
-	      return true;
-	    
-	    return false;
-	 }
+
+		Pattern p = Pattern
+				.compile("^[\\w-]+(\\.[\\w-]+)*@([\\w-]+\\.)+[a-zA-Z]{2,7}$");
+		Matcher m = p.matcher(email);
+
+		if (m.find())
+			return true;
+
+		return false;
+	}
 }
