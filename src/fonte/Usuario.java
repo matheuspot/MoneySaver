9cf0ce421050c78070fe66e6d01d10fd3be38d5d
Flábio Fagundes <flabio.fagundes@gmail.com>
2014-07-09 18:26:15 -0300

    Classes antigas deletadas e atualização

--- src/fonte/Usuario.java
+++ src/fonte/Usuario.java
@@ -1,5 +1,36 @@
 package fonte;
 
+import java.util.regex.*;
+
 public class Usuario {
 
+	private String nome, email, senha, dicaSenha;
+	
+	public Usuario(String nome, String email, String senha, String dicaSenha) throws Exception {
+		if ((nome.equals("")) || (nome == null))
+			throw new Exception("Nome nao definido.");
+		
+		if ((senha.length() < 6) || (senha.length() > 8))
+			throw new Exception("Senha com tamanho invalido.");
+		
+		if (!validaEmail(email))
+			throw new Exception("Email invalido.");
+		
+		this.nome = nome;
+		this.email = email;
+		this.senha = senha;
+		this.dicaSenha = dicaSenha;
+	}
+	
+	public boolean validaEmail(String email) {
+		if (email != null && email.length() > 0) 
+			return false;
+		
+	    Pattern p = Pattern.compile("^[\\w-]+(\\.[\\w-]+)*@([\\w-]+\\.)+[a-zA-Z]{2,7}$"); 
+	    Matcher m = p.matcher(email); 
+	    if (m.find())
+	      return true;
+	    
+	    return false;
+	 }
 }
