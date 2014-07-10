b136865ff114714683a7118e0ee1d19ce9039805
Roberto Matheus <matheus.pot@gmail.com>
2014-07-09 21:08:12 -0300

    Testes de GerenteDeUsuarios e configs eclipse

--- src/testes/TestaGerenteDeUsuarios.java
+++ src/testes/TestaGerenteDeUsuarios.java
@@ -1,13 +1,21 @@
 package testes;
 
 import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
+import fonte.GerenteDeUsuarios;
 
 public class TestaGerenteDeUsuarios {
 
+	private GerenteDeUsuarios gerente;
+
+	@Before
+	public void inicializaGerenteParaTestes() {
+		gerente = new GerenteDeUsuarios();
+	}
+
 	@Test
-	public void test() {
+	public void testAdicionaUsuarioValido() {
 		fail("Not yet implemented");
 	}
 
