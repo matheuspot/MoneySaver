30058471cb239756b5df04d01035134c2407916a
Thales Souto <thaleshenriquepb@gmail.com>
2014-08-06 11:53:12 -0300

    Mais mudanças em GerenteDeCategorias

--- src/fonte/GerenteDeCategorias.java
+++ src/fonte/GerenteDeCategorias.java
@@ -21,16 +21,18 @@ public class GerenteDeCategorias {
 		}		
 	}
 	
-	public void adicionaCategoria(String nomeCategoria) throws Exception {
-		categoriaValida(nomeCategoria);
-		Categoria categoria = new Categoria(nomeCategoria);
+	public void adicionaCategoria(String nomeCategoria, String corCategoria) throws Exception {
+		categoriaValida(nomeCategoria, corCategoria);
+		Categoria categoria = new Categoria(nomeCategoria, corCategoria);
 		listaCategorias.add(categoria);
 		arquivadorCategorias.escreveCategorias(listaCategorias);
 	}
 	
-	public void categoriaValida(String nomeCategoria) throws Exception {
+	public void categoriaValida(String nomeCategoria, String corCategoria) throws Exception {
 		if(nomeCategoria==null || nomeCategoria.length()==0)
 			throw new Exception("Nome da categoria está invalido.");
+		if(corCategoria==null || corCategoria.length()==0)
+			throw new Exception("Cor está invalida.");
 	}
 	
 
