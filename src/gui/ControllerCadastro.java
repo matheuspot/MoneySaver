ef819a23514b670c68329441c0456de97bb33264
Roberto Matheus <matheus.pot@gmail.com>
2014-08-09 17:46:36 -0300

    Telas de adicionar transação e tela principa depois de login.

--- src/gui/ControllerCadastro.java
+++ src/gui/ControllerCadastro.java
@@ -14,76 +14,78 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class ControllerCadastro {
-	
+
 	EventHandler<ActionEvent> eventos = (EventHandler<ActionEvent>) new Eventos();
-	
+
 	@FXML
-    private PasswordField PFsenha;
-    @FXML
-    private TextField TFdicaSenha;
-    @FXML
-    private PasswordField PFconfirmacaoSenha;
-    @FXML
-    private TextField TFnome;
-    @FXML
-    private TextField TFemail;
-    @FXML
-    private Button botaoCadastrar;
-    @FXML
-    private Button botaoVoltar;
-    @FXML
-    private Label labelAviso;
-    @FXML
-    private AnchorPane content;
-    
-    @FXML
-    void initialize() {
-    	TFnome.setOnAction(eventos);
-    	TFemail.setOnAction(eventos);
-    	PFsenha.setOnAction(eventos);
-    	TFdicaSenha.setOnAction(eventos);
-    	PFconfirmacaoSenha.setOnAction(eventos);
-    	botaoCadastrar.setOnAction(eventos);
-    	botaoVoltar.setOnAction(eventos);
-    	labelAviso.setVisible(false);
-    }
-    
-    private class Eventos implements EventHandler<ActionEvent>{
-    	
-    	GerenteDeUsuarios gerente = new GerenteDeUsuarios();
-    	
-    	@Override
-    	public void handle(ActionEvent evento){
-    		if (evento.getSource() == botaoCadastrar){
-    			 
-     	            try {
-     					gerente.adicionaUsuario(TFnome.getText(), TFemail.getText(), PFsenha.getText(), PFconfirmacaoSenha.getText(), TFdicaSenha.getText());
-     					Dialogs.create()
-     	    	        .owner(null)
-     	    	        .title("MoneySaver")
-     	    	        .masthead(null)
-     	    	        .message("Cadastro efetuado!")
-     	    	        .showInformation();
-     					try {
-     						content.getChildren().setAll(FXMLLoader.load(getClass().getResource("TelaPrincipal.fxml")));
-     		    			} catch (IOException e) {
-     		    				e.printStackTrace();
-     		    			}
-     					
-     	            } catch (Exception e) {
-     	            	labelAviso.setText(e.getMessage());
-     					labelAviso.setVisible(true);
-     				}
-     	     }
-    		
-    		if (evento.getSource() == botaoVoltar){
-    			try {
-				content.getChildren().setAll(FXMLLoader.load(getClass().getResource("TelaPrincipal.fxml")));
-    			} catch (IOException e) {
-    				e.printStackTrace();
-    			}
-    		}
-    		
-    	}
-    }
+	private PasswordField PFsenha;
+	@FXML
+	private TextField TFdicaSenha;
+	@FXML
+	private PasswordField PFconfirmacaoSenha;
+	@FXML
+	private TextField TFnome;
+	@FXML
+	private TextField TFemail;
+	@FXML
+	private Button botaoCadastrar;
+	@FXML
+	private Button botaoVoltar;
+	@FXML
+	private Label labelAviso;
+	@FXML
+	private AnchorPane content;
+
+	@FXML
+	void initialize() {
+		TFnome.setOnAction(eventos);
+		TFemail.setOnAction(eventos);
+		PFsenha.setOnAction(eventos);
+		TFdicaSenha.setOnAction(eventos);
+		PFconfirmacaoSenha.setOnAction(eventos);
+		botaoCadastrar.setOnAction(eventos);
+		botaoVoltar.setOnAction(eventos);
+		labelAviso.setVisible(false);
+	}
+
+	private class Eventos implements EventHandler<ActionEvent> {
+
+		GerenteDeUsuarios gerente = new GerenteDeUsuarios();
+
+		@Override
+		public void handle(ActionEvent evento) {
+			if (evento.getSource() == botaoCadastrar) {
+
+				try {
+					gerente.adicionaUsuario(TFnome.getText(),
+							TFemail.getText(), PFsenha.getText(),
+							PFconfirmacaoSenha.getText(), TFdicaSenha.getText());
+					Dialogs.create().owner(null).title("MoneySaver")
+							.masthead(null).message("Cadastro efetuado!")
+							.showInformation();
+					try {
+						content.getChildren().setAll(
+								FXMLLoader.load(getClass().getResource(
+										"TelaPrincipal.fxml")));
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+
+				} catch (Exception e) {
+					labelAviso.setText(e.getMessage());
+					labelAviso.setVisible(true);
+				}
+			}
+
+			if (evento.getSource() == botaoVoltar) {
+				try {
+					content.getChildren().setAll(
+							FXMLLoader.load(getClass().getResource(
+									"TelaPrincipal.fxml")));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
--- src/gui/ControllerTelaPrincipal.java
+++ src/gui/ControllerTelaPrincipal.java
@@ -14,49 +14,44 @@ import javafx.scene.layout.AnchorPane;
 public class ControllerTelaPrincipal {
 
 	EventHandler<ActionEvent> eventos = (EventHandler<ActionEvent>) new Eventos();
-	
-    @FXML
-    private Button botaoCadastrar;
-    @FXML
-    private Button botaoConectar;
-    @FXML
-    private PasswordField PFsenha;
-    @FXML
-    private TextField TFemail;
-    @FXML
-    private AnchorPane content;
-    
-    
-    @FXML
-    void initialize() {
-    	botaoCadastrar.setOnAction(eventos);
-    	botaoConectar.setOnAction(eventos);
-    	PFsenha.setOnAction(eventos);
-    	TFemail.setOnAction(eventos);
-    	
-    }
-    
-    private class Eventos implements EventHandler<ActionEvent>{
-    	
-    	@Override
-    	public void handle(ActionEvent evento){
-    		if (evento.getSource() == botaoCadastrar){
+
+	@FXML
+	private Button botaoCadastrar;
+	@FXML
+	private Button botaoConectar;
+	@FXML
+	private PasswordField PFsenha;
+	@FXML
+	private TextField TFemail;
+	@FXML
+	private AnchorPane content;
+
+	@FXML
+	void initialize() {
+		botaoCadastrar.setOnAction(eventos);
+		botaoConectar.setOnAction(eventos);
+		PFsenha.setOnAction(eventos);
+		TFemail.setOnAction(eventos);
+	}
+
+	private class Eventos implements EventHandler<ActionEvent> {
+
+		@Override
+		public void handle(ActionEvent evento) {
+			if (evento.getSource() == botaoCadastrar) {
 				try {
-					content.getChildren().setAll(FXMLLoader.load(getClass().getResource("TelaCadastrar.fxml")));
+					content.getChildren().setAll(
+							FXMLLoader.load(getClass().getResource(
+									"TelaCadastrar.fxml")));
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-    	    }
-    	    
-    		else if (evento.getSource() == botaoConectar){
-    			Dialogs.create()
-    	        .owner(null)
-    	        .title("MoneySaver")
-    	        .masthead(null)
-    	        .message("Botão conectar!")
-    	        .showInformation();
-    			
-    		}
-    	}
-    }
+			}
+
+			else if (evento.getSource() == botaoConectar) {
+				Dialogs.create().owner(null).title("MoneySaver").masthead(null)
+						.message("Botão conectar!").showInformation();
+			}
+		}
+	}
 }
