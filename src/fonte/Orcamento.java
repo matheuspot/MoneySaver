e710d11fd86d0a6fb88ce90d95f66615fc23538b
Roberto Matheus <matheus.pot@gmail.com>
2014-08-31 15:48:09 -0300

    Bug fix em Orcamento.

--- src/fonte/Orcamento.java
+++ src/fonte/Orcamento.java
@@ -1,12 +1,14 @@
 package fonte;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 /**
  * Classe usada para representar um orçamento.
  */
-public class Orcamento {
+public class Orcamento implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	private double limite;
 	private final int dataDeCriacao;
 
